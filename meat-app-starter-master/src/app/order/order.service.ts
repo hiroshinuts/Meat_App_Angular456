8f1ef6d8f67aae09362de304747d8e18d475ee3a
Hiro <hiroshi_nuts@hotmail.com>
2019-04-19 01:11:46 -0300

    Finalizando a Compra --HTTP POST ANGULAR 4--

--- meat-app-starter-master/src/app/order/order.service.ts
+++ meat-app-starter-master/src/app/order/order.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from "@angular/core";
+
+
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
+import { Order, OrderItem } from "./order.model";
+import { Observable } from "rxjs/Observable";
+import { Http, Headers, RequestOptions} from '@angular/http';
+import 'rxjs/add/operator/map'
+import { MEAT_API } from '../app.api';
+
 
 @Injectable()
 export class OrderService{
 
-    constructor(private cartService: ShoppingCartService){}
+    constructor(private cartService: ShoppingCartService, private http: Http){}
 
     itemsValue(): number {
         return this.cartService.total()
@@ -25,6 +33,19 @@ export class OrderService{
 
     remove(item: CartItem){
         this.cartService.removeItem(item)
+    }
+    
+    clear(){
+        this.cartService.clear()
+    }
+
+    checkOrder(order: Order): Observable<string>{
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`, 
+                            JSON.stringify(order),
+                            new RequestOptions({headers: headers}))
+                        .map(response => response.json())
     }
 
 }
