eee8a09aafca6447fee87dc39f02f35df4f5dc9c
Hiro <hiroshi_nuts@hotmail.com>
2019-04-23 18:14:20 -0300

    Aplicando Animacao no SnackBar - Aparecer e Sumir

--- meat-app-starter-master/src/app/shared/messages/snackbar/snackbar.component.ts
+++ meat-app-starter-master/src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,18 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'mt-snackbar',
   templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.css']
+  styleUrls: ['./snackbar.component.css'],
+  animations: [
+    trigger('snack-visibility', [
+      state('hidden', style({
+        opacity: 0,
+        botton: '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        botton: '30px'
+      })),
+      transition('hidden => visible', animate('500ms 0s ease-in')),
+      transition('visible => hidden', animate('500ms 0s ease-out'))
+    ])
+  ]
 })
 export class SnackbarComponent implements OnInit {
 
   message: string = 'Hello there!'
+
+  snackVisibility: string = 'hidden'
 
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  // Apenas para realizar Teste
+  // toggleSnack(){
+  //   this.snackVisibility = this.snackVisibility ==='hidden' ? 'visible' : 'hidden'
+  // }
+
 
 }
